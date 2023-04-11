03887b947210efdcb28f3ddef8785a9179c43c22
lucio nathan <lucio.matias@ccc.ufcg.edu.br>
2023-04-11 01:54:34 -0300

    Revert "modularizing the backend code to better reading"
    This reverts commit 8fabd56ab19bfdb63d43a32d49bdb9ceb7463748.

--- pomodoro-backend/cmd/web/main.go
+++ pomodoro-backend/cmd/web/main.go
@@ -4,101 +4,208 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"strconv"
-
-	"github.com/lucionathan/pomodoro/cmd/web/ws"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/lucionathan/pomodoro/cmd/web/session"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
+type Client struct {
+	conn *websocket.Conn
+}
+
+type Session struct {
+	id          string
+	clients     map[*Client]bool
+	broadcast   chan []byte
+	startTime   int64
+	elapsedTime int
+	quit        chan bool
+}
+
+func (s *Session) broadcastMessages() {
+	for {
+		select {
+		case message := <-s.broadcast:
+			for client := range s.clients {
+				err := client.conn.WriteMessage(websocket.TextMessage, message)
+				if err != nil {
+					client.conn.Close() // Close the connection
+					delete(s.clients, client)
+				}
+			}
+
+		case <-s.quit:
+			return
+		}
+	}
+}
+
+type Message struct {
+	Action      string `json:"action"`
+	Data        string `json:"data"`
+	StartTime   int64  `json:"startTime"`
+	ElapsedTime int    `json:"elapsedTime"`
+}
+
+var sessions = make(map[string]*Session)
+var sessionsMutex = &sync.Mutex{}
+
 func handleWebSocketCreate(w http.ResponseWriter, r *http.Request) {
-	conn, err := ws.UpgradeConnection(w, r)
+	conn, err := upgradeConnection(w, r)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	sessionID := session.GenerateRandomSessionID()
-	sess := session.GetSession(sessionID)
-	client := &session.Client{Conn: conn}
-	sess.Clients[client] = true
-	sendStartingTimestampAndElapsedTime(sess, client)
-	sendToSession(sess, "created", sessionID, client)
+	sessionID := generateRandomSessionID()
+	session := getSession(sessionID)
+	client := &Client{conn: conn}
+	session.clients[client] = true
+	sendStartingTimestampAndElapsedTime(session, client)
+	sendToSession(session, "created", sessionID, client)
 
-	readAndProcessMessages(conn, sess, client)
+	readAndProcessMessages(conn, session, client)
 }
 
 func handleWebSocketJoin(w http.ResponseWriter, r *http.Request) {
-	conn, err := ws.UpgradeConnection(w, r)
+	conn, err := upgradeConnection(w, r)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	client, sess := createClientAndSession(r, conn)
-	sess.Clients[client] = true
-	sendStartingTimestampAndElapsedTime(sess, client)
+	client, session := createClientAndSession(r, conn)
+	session.clients[client] = true
+	sendStartingTimestampAndElapsedTime(session, client)
 
-	readAndProcessMessages(conn, sess, client)
+	readAndProcessMessages(conn, session, client)
 }
 
-func createClientAndSession(r *http.Request, conn *websocket.Conn) (*session.Client, *session.Session) {
-	client := &session.Client{Conn: conn}
+func upgradeConnection(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	return conn, err
+}
+
+func createClientAndSession(r *http.Request, conn *websocket.Conn) (*Client, *Session) {
+	client := &Client{conn: conn}
 	sessionID := r.URL.Query().Get("session")
-	sess := session.GetSession(sessionID)
-	return client, sess
+	session := getSession(sessionID)
+	return client, session
 }
 
-func sendStartingTimestampAndElapsedTime(sess *session.Session, client *session.Client) {
-	if sess.StartTime > 0 || sess.ElapsedTime > 0 {
-		sendToSession(sess, "play", strconv.FormatInt(sess.StartTime, 10)+","+strconv.Itoa(sess.ElapsedTime), client)
+func sendStartingTimestampAndElapsedTime(session *Session, client *Client) {
+	if session.startTime > 0 || session.elapsedTime > 0 {
+		sendToSession(session, "play", strconv.FormatInt(session.startTime, 10)+","+strconv.Itoa(session.elapsedTime), client)
 	}
 }
 
-func readAndProcessMessages(conn *websocket.Conn, sess *session.Session, client *session.Client) {
+func readAndProcessMessages(conn *websocket.Conn, session *Session, client *Client) {
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
-			delete(sess.Clients, client)
-			session.CheckAndCloseSessionIfEmpty(sess)
+			delete(session.clients, client)
+			checkAndCloseSessionIfEmpty(session)
 			break
 		}
+		// fmt.Println(message)
 
-		var msg session.Message
+		var msg Message
 		json.Unmarshal(message, &msg)
 
 		fmt.Println(msg)
 
 		switch msg.Action {
 		case "pause":
-			sendToSession(sess, "pause", msg.Data, nil)
+			sendToSession(session, "pause", msg.Data, nil)
 		case "play":
-			sendToSession(sess, "play", msg.Data, nil)
+			sendToSession(session, "play", msg.Data, nil)
 		}
 	}
 }
 
-func sendToSession(sess *session.Session, action string, data string, targetClient *session.Client) {
-	session.CalculateSessionTime(sess, action)
+func checkAndCloseSessionIfEmpty(session *Session) {
+	if len(session.clients) == 0 {
+		session.quit <- true
+		sessionsMutex.Lock()
+		delete(sessions, session.id)
+		sessionsMutex.Unlock()
+	}
+}
 
-	message := session.Message{
+func calculateSessionTime(session *Session, action string) {
+	now := time.Now().UnixNano() / int64(time.Millisecond)
+	if action == "play" {
+		if session.startTime == 0 {
+			session.startTime = now
+		}
+	} else if action == "pause" {
+		if session.startTime != 0 {
+			elapsed := now - session.startTime
+			session.elapsedTime += int(elapsed) / 1000
+			session.startTime = 0
+		}
+	}
+}
+
+func sendToSession(session *Session, action string, data string, targetClient *Client) {
+	calculateSessionTime(session, action)
+
+	message := Message{
 		Action:      action,
 		Data:        data,
-		StartTime:   sess.StartTime,
-		ElapsedTime: sess.ElapsedTime,
+		StartTime:   session.startTime,
+		ElapsedTime: session.elapsedTime,
 	}
 	messageBytes, _ := json.Marshal(message)
 
 	if targetClient != nil {
-		err := targetClient.Conn.WriteMessage(websocket.TextMessage, messageBytes)
+		err := targetClient.conn.WriteMessage(websocket.TextMessage, messageBytes)
 		if err != nil {
-			delete(sess.Clients, targetClient)
+			delete(session.clients, targetClient)
 		}
 	} else {
-		sess.Broadcast <- messageBytes
+		session.broadcast <- messageBytes
 	}
+}
+
+func getSession(sessionID string) *Session {
+	sessionsMutex.Lock()
+	defer sessionsMutex.Unlock()
+	session, ok := sessions[sessionID]
+	if !ok {
+		session = &Session{
+			id:        sessionID,
+			clients:   make(map[*Client]bool),
+			broadcast: make(chan []byte),
+			quit:      make(chan bool),
+		}
+		go session.broadcastMessages()
+		sessions[sessionID] = session
+	}
+
+	return session
+}
+
+func generateRandomSessionID() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const length = 8
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(b)
 }
 
 func main() {
